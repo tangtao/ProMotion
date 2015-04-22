b3d1f2f1324c796ac697fc13c8b3f8f5a787832a
tangtao <tangtao@gmail.com>
2015-04-22 16:45:31 +0800

    add text_attributes and tint_color to table screen refreshable

--- lib/ProMotion/table/extensions/refreshable.rb
+++ lib/ProMotion/table/extensions/refreshable.rb
@@ -9,8 +9,12 @@ module ProMotion
         @updated_time_format = params[:updated_time_format] || "%l:%M %p"
         @refreshable_callback = params[:callback] || :on_refresh
 
+        @text_attributes = params[:text_attributes] || {}
+        @tint_color = params[:tint_color] || UIColor.lightGrayColor
+
         @refresh_control = UIRefreshControl.alloc.init
-        @refresh_control.attributedTitle = NSAttributedString.alloc.initWithString(pull_message)
+        @refresh_control.attributedTitle = create_refresh_attributed_string(pull_message)
+        @refresh_control.tintColor = @tint_color
         @refresh_control.addTarget(self, action:'refreshView:', forControlEvents:UIControlEventValueChanged)
         self.refreshControl = @refresh_control
       end
@@ -28,7 +32,7 @@ module ProMotion
       def end_refreshing
         return unless @refresh_control
 
-        @refresh_control.attributedTitle = NSAttributedString.alloc.initWithString(sprintf(@updated_format, Time.now.strftime(@updated_time_format)))
+        @refresh_control.attributedTitle = create_refresh_attributed_string(sprintf(@updated_format, Time.now.strftime(@updated_time_format)))
         @refresh_control.endRefreshing
       end
       alias :stop_refreshing :end_refreshing
@@ -37,13 +41,20 @@ module ProMotion
 
       # UIRefreshControl Delegates
       def refreshView(refresh)
-        refresh.attributedTitle = NSAttributedString.alloc.initWithString(@refreshing)
+        refresh.attributedTitle = create_refresh_attributed_string(@refreshing)
         if @refreshable_callback && self.respond_to?(@refreshable_callback)
           self.send(@refreshable_callback)
         else
           PM.logger.warn "You must implement the '#{@refreshable_callback}' method in your TableScreen."
         end
       end
+
+      private
+
+      def create_refresh_attributed_string(text)
+        NSAttributedString.alloc.initWithString(text, attributes: @text_attributes)
+      end
+
     end
   end
 end
